54d1638119334ee3537d2c37786f6dcc57fe5bb1
Evan Kwan <evanyuekwan@gmail.com>
2023-03-04 21:24:25 -0500

    charts tests

---
+++ src/pages/charts.spec.js
@@ -0,0 +1,24 @@
+import charts from "./charts.vue"
+
+import { mount, enableAutoUnmount } from "@vue/test-utils"
+import { createTestingPinia } from "@pinia/testing"
+import { describe, it, expect } from "vitest"
+import { useExpensesStore } from "../store/expenses"
+
+enableAutoUnmount(afterEach)
+
+describe("charts.vue", () => {
+  const getWrapper = () => mount(charts, {
+    shallow: true,
+    global: {
+      plugins: [createTestingPinia()],
+      stubs: ["PageHeader", "ExpensesOverTime"],
+    }
+  })
+  it("calls the getCategories action when mounting the component", async () => {
+    getWrapper();
+    const expensesStore = useExpensesStore()
+
+    expect(expensesStore.getCategories).toHaveBeenCalled()
+  })
+})
