3785dcc5cce5326057261410d83dc15851e88a17
Evan Kwan <evanyuekwan@gmail.com>
2023-03-04 21:02:45 -0500

    change time span to 12 months

--- src/components/ExpensesOverTime.vue
+++ src/components/ExpensesOverTime.vue
@@ -30,7 +30,7 @@ const props = defineProps({
   }
 })
 
-const possibleTimeSpan = computed(() => ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 18, 24]))
+const possibleTimeSpan = computed(() => ([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]))
 
 const currentMonth = new Date().getMonth()
 const setMonth = (e) => {
