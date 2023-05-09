804cb8b94cc9353b8280f38447f8a73a05f52f0c
Evan Kwan <evanyuekwan@gmail.com>
2023-05-08 21:41:08 -0400

    add categories route and page

---
+++ src/pages/categories.vue
@@ -0,0 +1,11 @@
+<script setup>
+
+</script>
+
+<template>
+  <div>
+    Test
+  </div>
+</template>
+
+<style lang="scss" scoped></style>
--- src/router/router.js
+++ src/router/router.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHashHistory } from "vue-router"
 const routes = [
   { path: "/", component: () => import("@/pages/welcome.vue") },
   { path: "/expenses", component: () => import("@/pages/expenses.vue") },
-  { path: "/charts", component: () => import("@/pages/charts.vue") }
+  { path: "/charts", component: () => import("@/pages/charts.vue") },
+  { path: "/categories", component: () => import("@/pages/categories.vue") },
 ]
 
 const router = createRouter({
