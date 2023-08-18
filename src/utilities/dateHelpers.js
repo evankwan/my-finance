ad4b9b5e1e828b42ff13a954d04c31cb9e2f033e
Evan Kwan <evanyuekwan@gmail.com>
2023-08-17 23:44:26 -0400

    more linting

--- src/utilities/dateHelpers.js
+++ src/utilities/dateHelpers.js
@@ -1,44 +1,44 @@
 export const getMonthName = (index) => {
 	let name;
 	switch (index) {
-		case 0:
-			name = "January"
-			break
-		case 1:
-			name = "February"
-			break
-		case 2:
-			name = "March"
-			break
-		case 3:
-			name = "April"
-			break
-		case 4:
-			name = "May"
-			break
-		case 5:
-			name = "June"
-			break
-		case 6:
-			name = "July"
-			break
-		case 7:
-			name = "August"
-			break
-		case 8:
-			name = "September"
-			break
-		case 9:
-			name = "October"
-			break
-		case 10:
-			name = "November"
-			break
-		case 11:
-			name = "December"
-			break
-		default:
-			break
+	case 0:
+		name = "January"
+		break
+	case 1:
+		name = "February"
+		break
+	case 2:
+		name = "March"
+		break
+	case 3:
+		name = "April"
+		break
+	case 4:
+		name = "May"
+		break
+	case 5:
+		name = "June"
+		break
+	case 6:
+		name = "July"
+		break
+	case 7:
+		name = "August"
+		break
+	case 8:
+		name = "September"
+		break
+	case 9:
+		name = "October"
+		break
+	case 10:
+		name = "November"
+		break
+	case 11:
+		name = "December"
+		break
+	default:
+		break
 	}
 	return name
 }
