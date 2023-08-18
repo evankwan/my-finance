0739e485604a21a3f27c77e871cc1c417f8abd52
Evan Kwan <evanyuekwan@gmail.com>
2023-08-17 23:29:45 -0400

    install eslint

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    "root": true,
+    "extends": [
+      "eslint:recommended",
+    ],
+    "rules": {
+      "array-bracket-spacing": "always",
+      "arrow-spacing": "always",
+      "block-spacing": "always",
+      "indent": [
+        "error",
+        "tab"
+      ],
+      "no-duplicate-imports": "error",
+      "no-param-reassign": "error"
+    }
+  }
+]
