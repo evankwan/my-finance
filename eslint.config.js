ee11dca201bd55d0656a5106bea827d271357116
Evan Kwan <evanyuekwan@gmail.com>
2023-08-26 13:40:24 -0400

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,37 +1,54 @@
-import globals from "globals";
-import eslintPluginVue from "eslint-plugin-vue"
+import { FlatCompat } from "@eslint/eslintrc"
+import path from "path";
+import { fileURLToPath } from "url";
+
+import globals from "globals"
+import vue from "eslint-plugin-vue"
 import vueParser from "vue-eslint-parser"
 
-const { base: vueBase, recommended: vueRecommended } = eslintPluginVue.configs
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const compat = new FlatCompat({ baseDirectory: __dirname })
+
+const { base: vueBase, "vue3-recommended": vueRecommended, } = vue.configs
 
 export default [
+	{
+		ignores: ["node_modules", "coverage", "dist"],
+	},
+	...compat.plugins("vue"),
+	...compat.extends("plugin:vue/vue3-strongly-recommended"),
 	{
 		files: [
 			"src/**/*.{js,vue}",
 		],
-		plugins: {
-			"globals/browser": globals.browser,
-			"plugin:vue/base": vueBase,
-			"plugin:vue/vue3-recommended": vueRecommended,
+		languageOptions: {
+			globals: {
+				...globals.browser,
+			},
+			parser: vueParser,
+			parserOptions: {
+				ecmaVersion: "latest",
+				sourceType: "module",
+			},
 		},
-		// 	globals: globals.browser,
-		// 	vue: eslintPluginVue,
-		// },
-		"languageOptions": {
-			"parser": vueParser,
-		},
-		"rules": {
+		rules: {
 			"array-bracket-spacing": 2,
 			"arrow-spacing": 2,
 			"block-spacing": 2,
 			"indent": [
 				"error",
-				"tab"
+				4
 			],
 			"no-duplicate-imports": "error",
 			"no-multi-spaces": 2,
 			"no-param-reassign": "error",
-
+			"vue/singleline-html-element-content-newline": ["error", {
+				"ignoreWhenNoAttributes": true,
+				"ignoreWhenEmpty": true,
+				"ignores": ["pre", "textarea"]
+			}],
+			"vue/multi-word-component-names": "off",
 		}
 	}
 ]
