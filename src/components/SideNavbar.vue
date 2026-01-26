620cd9eb49076ff23ce3d42c7d3fbd288a4f2c9a
evankwan <evanyuekwan@gmail.com>
2026-01-26 09:59:56 -0500

    add basic routing for budget page

--- src/components/SideNavbar.vue
+++ src/components/SideNavbar.vue
@@ -1,47 +1,48 @@
 <script setup lang="ts">
-import Menu from "primevue/menu"
+import Menu from "primevue/menu";
 
 const items = [
-  {
-      label: "Home",
-      route: "/"
-  },
-  {
-      label: "Categories",
-      route: "/categories"
-  },
-  {
-      label: "Monthly recap",
-      route: "/monthly-recap"
-  },
-]
+	{
+		label: "Home",
+		route: "/",
+	},
+	{
+		label: "Categories",
+		route: "/categories",
+	},
+	{
+		label: "Budget",
+		route: "/budget",
+	},
+	{
+		label: "Monthly recap",
+		route: "/monthly-recap",
+	},
+];
 </script>
 
 <template>
-  <nav class="side-navbar">
-    <Menu :model="items">
-      <template #item="{ item }">
-        <router-link
-          :to="item.route"
-          class="navbar-link"
-        >
-          {{ item.label }}
-        </router-link>
-      </template>
-    </Menu>
-  </nav>
+	<nav class="side-navbar">
+		<Menu :model="items">
+			<template #item="{ item }">
+				<router-link :to="item.route" class="navbar-link">
+					{{ item.label }}
+				</router-link>
+			</template>
+		</Menu>
+	</nav>
 </template>
 
 <style scoped>
 .side-navbar {
-  padding: 0.5rem;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
+	padding: 0.5rem;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	align-items: flex-start;
 }
 .navbar-link {
-  display: inline-block;
-  width: 100%;
+	display: inline-block;
+	width: 100%;
 }
 </style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,33 +1,41 @@
-import { createMemoryHistory, createRouter } from "vue-router"
+import { createMemoryHistory, createRouter } from "vue-router";
 
 const routes = [
-  {
-    name: "home",
-    path: "/",
-    component: () => import("../pages/expenses-list.vue"),
-    meta: {
-      title: "Expenses",
-    },
-  },
-  {
-    name: "categories",
-    path: "/categories",
-    component: () => import("../pages/categories.vue"),
-    meta: {
-      title: "Categories",
-    },
-  },
-  {
-    name: "monthly-recap",
-    path: "/monthly-recap",
-    component: () => import("../pages/monthly-recap.vue"),
-    meta: {
-      title: "Monthly recap",
-    },
-  },
-]
+	{
+		name: "home",
+		path: "/",
+		component: () => import("../pages/expenses-list.vue"),
+		meta: {
+			title: "Expenses",
+		},
+	},
+	{
+		name: "categories",
+		path: "/categories",
+		component: () => import("../pages/categories.vue"),
+		meta: {
+			title: "Categories",
+		},
+	},
+	{
+		name: "budget",
+		path: "/budget",
+		component: () => import("../pages/budget.vue"),
+		meta: {
+			title: "Monthly budget",
+		},
+	},
+	{
+		name: "monthly-recap",
+		path: "/monthly-recap",
+		component: () => import("../pages/monthly-recap.vue"),
+		meta: {
+			title: "Monthly recap",
+		},
+	},
+];
 
 export const router = createRouter({
-  history: createMemoryHistory(),
-  routes,
-})
+	history: createMemoryHistory(),
+	routes,
+});
